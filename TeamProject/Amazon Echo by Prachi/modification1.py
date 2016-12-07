3e71a6e647ebd97bbb599de90e2dc494474825ac
PrachiHada <hada.prachi@gmail.com>
2016-12-07 15:28:22 -0800

    input name

---
+++ TeamProject/Amazon Echo by Prachi/modification1.py
@@ -0,0 +1,15 @@
+ {
+      "intent": "CustomerName",
+      "slots":
+      [
+          {
+            "name": "CustName",
+            "type": "AMAZON.US_FIRST_NAME"
+          }
+      ]
+    }, 
+
+    CustomerName {CustName}
+
+
+    Output speech = "Please tell your name"
