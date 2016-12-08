510388dd5c4bbcc586393686b0bd380eb31d2dae
PrachiHada <hada.prachi@gmail.com>
2016-12-07 16:13:45 -0800

    name and address

--- TeamProject/Amazon Echo by Prachi/modification1.py
+++ TeamProject/Amazon Echo by Prachi/modification1.py
@@ -1,4 +1,59 @@
+    {
+      "intent": "Customerdetails",
+      "slots":
+      [
+          {
+            "name": "CustName",
+            "type": "AMAZON.US_FIRST_NAME"
+          }
+      ]
+    },   
  {
+      "intent": "CustomerCity",
+      "slots":
+      [
+          {
+            "name": "City",
+            "type": "AMAZON.US_CITY"
+          }
+      ]
+    }, 
+
+CustomerName {CustName}
+CustomerCity {City}
+
+
+    Output speech = "Please tell your name"
+----------------------------------------------------------
+     {
+      "intent": "Customerdetails",
+      "slots":
+      [
+        {
+          "name": "CustName",
+          "type": "AMAZON.US_FIRST_NAME"
+        },
+        {
+          "name": "City",
+          "type": "AMAZON.US_CITY"
+        }
+      ]
+    },
+
+
+
+Customerdetails {CustName} and {City}
+Customerdetails {CustName} {City}
+Customerdetails {City} and {CustName}
+Customerdetails {City} {CustName}
+Customerdetails {CustName} and {City}
+Customerdetails {CustName} {City}
+Customerdetails {CustName}
+Customerdetails {City}
+
+  
+
+    {
       "intent": "CustomerName",
       "slots":
       [
@@ -7,9 +62,38 @@
             "type": "AMAZON.US_FIRST_NAME"
           }
       ]
-    }, 
+    },  
+    {
+      "intent": "Customerdetails",
+      "slots":
+      [
+        {
+          "name": "CustStreet",
+          "type": "AMAZON.StreetAddress"
+        },
+        {
+          "name": "CustAddress",
+          "type": "AMAZON.PostalAddress"
+        },
+        {
+          "name": "City",
+          "type": "AMAZON.US_CITY"
+        }
+      ]
+    },
+      
 
-    CustomerName {CustName}
+CustomerName {CustName}
+CustomerName {CustName} {CustName}
 
-
-    Output speech = "Please tell your name"
+Customerdetails {CustStreet} {CustAddress} {City}
+Customerdetails Address {CustStreet} {CustAddress} City {City}
+Customerdetails from {City}
+Customerdetails Address {CustStreet} {CustAddress} City {City}
+Customerdetails from {CustStreet} {CustName} 
+Customerdetails {CustStreet} {CustAddress} {City}
+Customerdetails {CustStreet} {CustAddress}
+Customerdetails {CustAddress} {City}
+Customerdetails {CustStreet} {City}
+Customerdetails {CustAddress} {CustStreet} {City}
+Customerdetails {City}
